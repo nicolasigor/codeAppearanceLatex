70c4c12360208b7f8f169b8f1cebda73546afc53
Nicolás Tapia <n.tapia.in@gmail.com>
2017-10-14 01:29:34 -0700

    Add files via upload

---
+++ leastSquares.jl
@@ -0,0 +1,13 @@
+include("misc.jl")
+
+function leastSquares(X,y)
+
+	# Find regression weights minimizing squared error
+	w = (X'*X)\(X'*y)
+
+	# Make linear prediction function
+	predict(Xhat) = Xhat*w
+
+	# Return model
+	return GenericModel(predict)
+end
